6ddff8901c0de2835140d99f43fc62bc90031d4d
daxarx <158153407+daxarx@users.noreply.github.com>
2024-07-06 00:15:08 +0500

    Update __main__.py

--- hikka/__main__.py
+++ hikka/__main__.py
@@ -46,7 +46,7 @@ else:
         try:
             import telethon
 
-            if tuple(map(int, telethon.__version__.split("."))) < (1, 24, 12):
+            if tuple(map(int, telethon.__version__.split("."))) < (1, 24, 14):
                 raise ImportError
         except ImportError:
             print("🔄 Installing Hikka-TL...")
